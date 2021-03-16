f7efbaf76908803f5e5ce3458af1e82d6f1800b8
DELL <rahulmishra0117@gmail.com>
2021-03-16 11:27:33 -0400

    all file are updated

--- JS/index.js
+++ JS/index.js
@@ -12,7 +12,59 @@ for(var i=0;i<navMenuAnchorTags.length;i++){
                 return ;
 
             }
-            window.scrollBy(0,50);
+            window.scrollBy(0,10);
         },20);
     });
 }
+
+// Skill auto filling section
+var progressBars = document.querySelectorAll(".skill-progress > div");
+var skillsContainer = document.getElementById('skills-container');
+var animationDone = false;
+
+
+
+function initialiseBars() {
+    for (var bar of progressBars) {
+        bar.style.width = 0 + '%';
+    }
+}
+
+initialiseBars();
+
+
+
+function fillBars() {
+
+    for (let bar of progressBars) {
+        let currentWidth = 0;
+        let interval = setInterval(function () {
+            let targetWidth = bar.getAttribute('data-bar-width');
+            if (currentWidth >= targetWidth) {
+                clearInterval(interval);
+                return;
+            }
+            currentWidth++;
+            bar.style.width = currentWidth + '%';
+        }, 5);
+    }
+}
+
+
+
+function checkScroll() {
+
+    var coordinates = skillsContainer.getBoundingClientRect();
+    if (!animationDone && coordinates.top <= window.innerHeight) {
+        animationDone = true;
+        fillBars();
+    } else if (coordinates.top > window.innerHeight) {
+        animationDone = false;
+        initialiseBars();
+    }
+}
+
+
+
+window.addEventListener("scroll", checkScroll);
+
